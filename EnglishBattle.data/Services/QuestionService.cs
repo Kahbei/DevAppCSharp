ca8d9d696292e5d74c105d24be3a3ee33f11cdee
Kahbei <mengus.gary@gmail.com>
2021-02-20 16:26:31 +0100

    Update QuestionService Put the CRUD logic in the file.

--- EnglishBattle.data/Services/QuestionService.cs
+++ EnglishBattle.data/Services/QuestionService.cs
@@ -8,6 +8,70 @@ namespace EnglishBattle.data.Services
 {
     class QuestionService
     {
+        private EnglishBattleEntities context;
 
+        /// <summary>
+        /// Get a Question following the id picked
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Question by the id</returns>
+        public Question GetQuestion(int id)
+        {
+            using (context)
+            {
+                return context.Question.Find(id);
+            }
+        }
+
+        /// <summary>
+        /// Get a Question list
+        /// </summary>
+        /// <returns>Question list</returns>
+        public List<Question> GetQuestionList()
+        {
+            using (context)
+            {
+                return context.Question.ToList();
+            }
+        }
+
+        /// <summary>
+        /// Add a Question
+        /// </summary>
+        /// <param name="question"></param>
+        public void InsertQuestion(Question question)
+        {
+            using (context)
+            {
+                context.Question.Add(question);
+                context.SaveChanges();
+            }
+        }
+
+        /// <summary>
+        /// Update a question
+        /// </summary>
+        /// <param name="question"></param>
+        public void UpdateQuestion(Question question)
+        {
+            using (context)
+            {
+                context.Entry(question).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+
+        /// <summary>
+        /// Delete a question
+        /// </summary>
+        /// <param name="question"></param>
+        public void DeleteQuestion(Question question)
+        {
+            using (context)
+            {
+                context.Entry(question).State = System.Data.Entity.EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
     }
 }
