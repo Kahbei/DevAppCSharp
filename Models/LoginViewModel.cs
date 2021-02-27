4a143a5b5ba3bd17e15604e9179279426f531e3f
Kahbei <mengus.gary@gmail.com>
2021-02-27 14:53:38 +0100

    Create ViewModel Login, Question & Register ViewModel create. The Register have to be a bit more completed and the Question has to be fill.

---
+++ Models/LoginViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EnglishBattleApp.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Courrier électronique")]
+        [EmailAddress] // si email valide
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Mot de passe")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
---
+++ Models/RegisterViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EnglishBattleApp.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [Display(Name = "Nom")] // utiliser dans le label associé au champ
+        public string Nom { get; set; }
+
+        [Required]
+        [Display(Name = "Prénom")]
+        public string Prenom { get; set; }
+
+        [Required]
+        [Display(Name = "Courrier électronique")]
+        [EmailAddress] // si email valide
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Mot de passe")]
+        [DataType(DataType.Password)]
+        [StringLength(14, ErrorMessage = "Le mot de passe doit être compris entre entre {2} et {1}", MinimumLength = 6)]
+        public string Password { get; set; }
+
+        [Required]
+        [Display(Name = "Confirmer le mot de passe")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Le mot de passe de correspond pas")]
+        public string ConfirmPassword { get; set; }
+    }
+}
